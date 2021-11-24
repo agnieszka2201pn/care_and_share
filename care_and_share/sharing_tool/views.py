5a5214dd5fa53586281c90814effb7d02fa529e7
Agnieszka <agnieszka.blicharska@gmail.com>
2021-11-24 21:11:27 +0100

    linki i dynamiczne wyświetlanie instytucji

--- care_and_share/sharing_tool/views.py
+++ care_and_share/sharing_tool/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.views import View
 
+from sharing_tool.models import Institution, Donation
+
+
 class LandingPage(View):
     def get(self,request):
-        return render(request, 'sharing_tool/index.html')
+        donations = Donation.objects.all()
+        bags_count = 0
+        for donation in donations:
+            bags_count += donation.quantity
+        organizations_count = Institution.objects.count()
+        foundations = Institution.objects.filter(type=0)
+        ngos = Institution.objects.filter(type=1)
+        foundraisings = Institution.objects.filter(type=2)
+        context = {'bags_count':bags_count,
+                   'organizations_count':organizations_count,
+                   'foundations':foundations,
+                   'ngos':ngos,
+                   'foundraisings':foundraisings,
+                   }
+        return render(request, 'sharing_tool/index.html', context)
 
 
 class AddDonation(View):
